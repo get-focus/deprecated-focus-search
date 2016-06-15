422b65b4d274c78eff0f7aa84ef5b14fcb35c016
Pierre Besson <pierre.besson7@gmail.com>
2016-06-15 12:12:37 +0200

    [root][add all providers]

--- README.md
+++ README.md
@@ -498,3 +498,39 @@ export default compose(
   connectToMetadata(['user','address','financialMove'])
 )(App);
 ```
+> En pratique nous n'allons pas connecter le composant d'application au store. Nous allons plutôt travailler sur une page qui va afficher les éléments prévus.
+
+## Faire une page avec la nouvelle ma manière de gérer le form.
+
+- Avant de commencer, pour pouvoir utiliser le form il faut placer d'autres provider
+- FieldHelpersProvider => Qui va nous servir à pouvoir récupérer les helper de form dans chacun des composantrs
+- MasterDataProvider => Qui va nous servir à injecter des listes de références dans certaines parties de l'application.
+
+Dans le composant root.
+
+```jsx
+import {Provider as MetadataProvider} from 'focus-redux/behaviours/metadata';
+import {Provider as FieldHelpersProvider} from 'focus-redux/behaviours/field';
+import {Provider as MasterDataProvider} from 'focus-redux/behaviours/master-data';
+//...
+const Root = ({store, history}) => /*On place le provider de store au plus haut afin de pouvoir injecter des informations du store dans toute l'applciation.*/
+<StoreProvider store={store}>
+  <MetadataProvider definitions={definitions} domains={domains}>
+    <FieldHelpersProvider >
+      <MasterDataProvider>
+        <Router history={history}>
+          {/* On injecte comme composant d'application un composant connecté au store redux */}
+            {/* Le composant IndexRoute signifie qui sera appellée par défaut*/}
+            <Route path='/' component={App} >
+            <IndexRoute component={Home}/>
+            {/* Les :id sert à fournir un paramètre à l'url on extrait les paramètres d'url via la props params*/}
+            <Route path='user/:id' component={({params}) => <User id={params.id}/>} />
+          </Route>
+        </Router>
+      </MasterDataProvider>
+      </FieldHelpersProvider>
+  </MetadataProvider>
+</StoreProvider>;
+```
+
+> Le socle applicatif est maintenant prêt.
--- src/root.js
+++ src/root.js
@@ -2,6 +2,9 @@ import React, {PropTypes} from 'react';
 import {IndexRoute, Router, Route} from 'react-router';
 import {Provider as StoreProvider} from 'react-redux';
 import {Provider as MetadataProvider} from 'focus-redux/behaviours/metadata';
+import {Provider as FieldHelpersProvider} from 'focus-redux/behaviours/field';
+import {Provider as MasterDataProvider} from 'focus-redux/behaviours/master-data';
+
 import * as definitions from './config/entity-definitions';
 import * as domains from './config/domains';
 /* Components */
@@ -13,15 +16,19 @@ import User from './views/user';
 const Root = ({store, history}) => /*On place le provider de store au plus haut afin de pouvoir injecter des informations du store dans toute l'applciation.*/
 <StoreProvider store={store}>
   <MetadataProvider definitions={definitions} domains={domains}>
-    <Router history={history}>
-      {/* On injecte comme composant d'application un composant connecté au store redux */}
-        {/* Le composant IndexRoute signifie qui sera appellée par défaut*/}
-        <Route path='/' component={App} >
-        <IndexRoute component={Home}/>
-        {/* Les :id sert à fournir un para  sssssssmètre à l'url on extrait les paramètres d'url via la props params*/}
-        <Route path='user/:id' component={({params}) => <User id={params.id}/>} />
-      </Route>
-    </Router>
+    <FieldHelpersProvider >
+      <MasterDataProvider>
+        <Router history={history}>
+          {/* On injecte comme composant d'application un composant connecté au store redux */}
+            {/* Le composant IndexRoute signifie qui sera appellée par défaut*/}
+            <Route path='/' component={App} >
+            <IndexRoute component={Home}/>
+            {/* Les :id sert à fournir un paramètre à l'url on extrait les paramètres d'url via la props params*/}
+            <Route path='user/:id' component={({params}) => <User id={params.id}/>} />
+          </Route>
+        </Router>
+      </MasterDataProvider>
+      </FieldHelpersProvider>
   </MetadataProvider>
 </StoreProvider>;
 
