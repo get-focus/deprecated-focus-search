f13601fd72af304eccd19646478c2c20b0f196c8
Ephrame <viollet.ame@gmail.com>
2016-06-16 19:34:10 +0200

    comments

--- src/reducer/user-reducer.js
+++ src/reducer/user-reducer.js
@@ -2,16 +2,18 @@ import {reducerBuilder} from 'focus-redux/reducers/reducer-builder';
 import {loadUserTypes} from '../actions/user-actions';
 import {saveUserTypes} from '../actions/user-actions';
 
-
+// Récupération des types des trois actions redux créé par l'actionBuilder
 const {REQUEST_LOAD_USER, RESPONSE_LOAD_USER, ERROR_LOAD_USER} = loadUserTypes;
+
+// Récupération des types des trois actions redux créé par l'actionBuilder
 const {REQUEST_SAVE_USER, RESPONSE_SAVE_USER, ERROR_SAVE_USER} = saveUserTypes;
 
-
+// Données initiales pour la state redux
 const DEFAULT_DATA = {
     firstName:'UserYolo'
 };
 
-
+// Utilisation du reducerBuilder qui attends le type des trois actions créés par l'actionBuimlder
 const userReducer = reducerBuilder({
     types: {
         load: {request: REQUEST_LOAD_USER, response: RESPONSE_LOAD_USER, error: ERROR_LOAD_USER},
