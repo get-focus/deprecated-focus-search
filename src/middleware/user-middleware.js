55a796434dd8ceaec922371d4e4e479719dc4824
Ephrame <viollet.ame@gmail.com>
2016-06-21 20:23:51 +0200

    Middleware maison

--- src/middleware/user-middleware.js
+++ src/middleware/user-middleware.js
@@ -1,13 +1,16 @@
 import builder from 'focus-redux/store/builder';
 import rootReducer from '../reducer';
-import {INPUT_CHANGE} from 'focus-redux/actions/input';
+import {INPUT_CHANGE, INPUT_ERROR} from 'focus-redux/actions/input';
 
 
-const lastNameMiddleware = store => next => action => {
-    if (action.type === INPUT_CHANGE && action.fieldName == 'firstName') {
+export const amoutToUpperCaseMiddleware = store => next => action => {
+    const {forms, definitions, domains} = store.getState();
+    if (action.type === INPUT_CHANGE && action.fieldName == 'amount') {
+        const {formKey} = action;
+        const {fields} = forms.find(f=> f.formKey === formKey);
         const lastNameAction = {...action};
-        lastNameAction.fieldName = 'lastName';
-        lastNameAction.rawValue = action.rawValue.toUpperCase();
+        lastNameAction.fieldName = 'name';
+        lastNameAction.rawValue =  fields.find(f => f.name == 'name').rawInputValue.toUpperCase();
         next(action);
         store.dispatch(lastNameAction);
     } else {
@@ -15,4 +18,19 @@ const lastNameMiddleware = store => next => action => {
     }
 }
 
-export default lastNameMiddleware;
+
+export const errorFieldMiddleware = store => next => action => {
+    const {forms, definitions, domains} = store.getState();
+    if (action.type === INPUT_CHANGE && action.fieldName == 'amount') {
+        const errorAction = {};
+        errorAction.type = 'INPUT_ERROR';
+        errorAction.formKey = action.formKey;
+        errorAction.fieldName = 'name';
+        errorAction.entityPath = action.entityPath;
+        errorAction.error = "Une erreur venue de l'espace !! "
+        next(action);
+        store.dispatch(errorAction);
+    } else {
+        next(action);
+    }
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import builder from 'focus-redux/store/builder';
 import rootReducer from '../../src/reducer';
-import lastNameMiddleware from '../../src/middleware/user-middleware';
+import {amoutToUpperCaseMiddleware, errorFieldMiddleware} from '../../src/middleware/user-middleware';
 
-const store = builder(rootReducer, [lastNameMiddleware]);
+const store = builder(rootReducer, [errorFieldMiddleware]);
 
 export default store;
