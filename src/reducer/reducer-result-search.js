468c113d9e63ea3335aad06c0491b0cec9e3c7d5
Ephrame <viollet.ame@gmail.com>
2016-07-07 12:23:30 +0200

    selector of the state

--- src/reducer/reducer-result-search.js
+++ src/reducer/reducer-result-search.js
@@ -33,3 +33,15 @@ export const unitResultsSearchReducerBuilder = name => (state = [], action) => {
   }
 
 }
+
+
+// It  extracts data from the dataset part of the state
+export const selectSearchResultByName = (searchName, name) => (state ={}) => {
+  if( !state[searchName] || !state[searchName].results[name] ) throw new Error(`SELECTOR_RESULTS_SEARCH : there is no ${searchName} or ${name} in the state`);
+  return state[searchName].results[name]
+}
+
+export const selectSearchResult = (searchName) => (state ={}) => {
+  if( !state[searchName] || !state[searchName].results) throw new Error(`SELECTOR_RESULTS_SEARCH : there is no ${searchName}  in the state`);
+  return state[searchName].results
+}
