75a98b62cdceda3e50573a73487e68ddf1c3f05e
Pierre Besson <pierre.besson7@gmail.com>
2016-08-08 17:59:32 +0200

    [selectable-list] add this as a beahviour with a connector <3

--- src/components/selectable-list.js
+++ src/components/selectable-list.js
@@ -5,6 +5,7 @@ const DATA_TEST = [
 ]
 
 function PureSelectableList(props){
+  // TODO: add a component with explaination on how to do the selectable connxion.
   return <ul>
     hello
     {
@@ -12,7 +13,7 @@ function PureSelectableList(props){
       ({isSelected, ...lineData}) => <li key={lineData[props.lineIdentifierProperty]}>
           {'IsSelected:'} {isSelected ? '[x]' : '[ ]'} -
           {JSON.stringify(lineData)}
-          <button onClick={() => props.toggleLine(lineData[props.lineIdentifierProperty])}>{'ToggleLine'}</button>
+          <button onClick={() => props.toggleLineSelection(lineData[props.lineIdentifierProperty])}>{'ToggleLine'}</button>
         </li>
       )
     }</ul>
@@ -22,6 +23,18 @@ PureSelectableList.propTypes = {
   lineIdentifierProperty: PropTypes.string.isRequired
 }
 
+
+const SELECTABLE_LIST_PROPTYPES = {
+  data: PropTypes.array,
+  lineIdentifierProperty: PropTypes.string.isRequired,
+  afterSelection: PropTypes.func
+};
+
+const SELECTABLE_LIST_DEFAULT_PROPS = {
+  data: DATA_TEST, //[]
+  lineIdentifierProperty: 'id',
+  afterSelection: () => {}
+};
 
 function addSelectedInformationInList(dataList = [], selectedElements = {}, lineIdentifierProperty = 'id'){
   return dataList.map(
@@ -36,46 +49,64 @@ function toggleLineSelection(selectedElements, elementId){
   return {...selectedElements, ...{[elementId] : !selectedElements[elementId]}};
 }
 
-class SelectableList extends Component {
-  constructor(props){
-      super(props);
-      this.state = {
-        selectedElements: {}
-      };
+/**
+ * Convert a list into a selectable list.
+ * It does two things =>
+ * - Add a `isSelected` props into the line data.
+ * - Add  a `toggleLineSelection` sort of action which call the parent state and change the selected information.
+ * Warning: Your list should have a props lineIdentifierPropert, see SELECTABLE_LIST_PROPTYPES. And the data shoud be an array.
+ *Example
+function PureSelectableListCustom({data, lineIdentifierProperty, toggleLineSelection}){
+   return <ul>
+     {
+       data.map(
+       ({isSelected, ...lineData}) => <li key={lineData[lineIdentifierProperty]}>
+          {isSelected ? '[x]' : '[ ]'} -
+           {JSON.stringify(lineData)}
+           <button onClick={() => toggleLineSelection(lineData[lineIdentifierProperty])}>{'ToggleLine'}</button>
+         </li>
+       )
+     }</ul>
+ }
+ *
+*/
+const connect = (ListToConnect = PureSelectableList) => {
+  class SelectableList extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+          selectedElements: {}
+        };
+    }
+    toggleLineSelectionInState(action){
+      //console.log('action', this.state, action)
+      this.setState({
+          selectedElements: toggleLineSelection(this.state.selectedElements, action)
+        },
+        this.props.afterSelection.bind(this)
+      );
+    }
+    render(){
+      const {data, lineIdentifierProperty} = this.props;
+      const dataWithSelectedInformation = addSelectedInformationInList(
+        data,
+        this.state.selectedElements,
+        lineIdentifierProperty
+      );
+      return(
+      <ListToConnect
+        data={dataWithSelectedInformation}
+        toggleLineSelection={this.toggleLineSelectionInState.bind(this)}
+        lineIdentifierProperty={lineIdentifierProperty}
+      />);
+    }
   }
-  toggleLineSelectionInState(action){
-    //console.log('action', this.state, action)
-    this.setState({
-        selectedElements: toggleLineSelection(this.state.selectedElements, action)
-      },
-      this.props.afterSelection.bind(this)
-    );
-  }
-  render(){
-    const {data, lineIdentifierProperty} = this.props;
-    const dataWithSelectedInformation = addSelectedInformationInList(
-      data,
-      this.state.selectedElements,
-      lineIdentifierProperty
-    );
-    return(
-    <PureSelectableList
-      data={dataWithSelectedInformation}
-      toggleLine={this.toggleLineSelectionInState.bind(this)}
-      lineIdentifierProperty={lineIdentifierProperty}
-    />);
-  }
+  SelectableList.displayName = `connectSelectableList${ListToConnect.name}`;
+  SelectableList.propTypes = SELECTABLE_LIST_PROPTYPES;
+  SelectableList.defaultProps = SELECTABLE_LIST_DEFAULT_PROPS;
+  return SelectableList;
 }
 
-SelectableList.propTypes =  {
-  data: PropTypes.array,
-  lineIdentifierProperty: PropTypes.string.isRequired,
-  afterSelection: PropTypes.func
-}
-SelectableList.defaultProps = {
-  data: DATA_TEST, //[]
-  lineIdentifierProperty: 'id',
-  afterSelection: () => {console.log('lol je suis sélectionné')}
-}
 
-export default SelectableList;
+
+export default connect();
