f1e0642da437cbfed18a6dc2c1d1bdd4cce631e9
Ephrame <viollet.ame@gmail.com>
2016-08-24 18:47:42 +0200

    fix rebase

--- src/example/root.js
+++ src/example/root.js
@@ -1,4 +1,4 @@
-import React, {PropTypes, Component} from 'react';
+import React, {PropTypes} from 'react';
 import {IndexRoute, Router, Route} from 'react-router';
 import Home from './views/home';
 import App from './app'
@@ -7,15 +7,15 @@ import 'babel-polyfill';
 import {Provider as SearchProvider} from '../behaviours/search';
 
 /* Components */
-const _getLineComponentFromContentTypeExample = (contentType, listData) => {
+const _getListMetadata = (contentType, listData) => {
   switch (contentType) {
     case 'DonDiegoType':
       return {
         LineComponent: props => {
           const color = props.isSelected ? 'orange' : 'blue'
-          return <div style={{color: color}}>
+          return <div style={{color: 'tomato'}}>
+                <input type='checkbox' checked={props.isSelected} onClick={() => props.toggleLineSelection(props.id)}/>
                 Line DonDiegoType {JSON.stringify(props)}
-                <button onClick={() => props.toggleLineSelection(props.id)}>action</button>
                 </div>
         },
         sortList : [
@@ -53,26 +53,20 @@ const _getLineComponentFromContentTypeExample = (contentType, listData) => {
   }
 }
 
-class Root extends Component {
-    render() {
-        const {store, history} = this.props;
-        return (
-            <StoreProvider store={store}>
-              <SearchProvider store={store} searchMetadata={{getLineComponentFromContentTypeExample : _getLineComponentFromContentTypeExample, sortList: ['ouaaaaah', 'ceeeeest', 'biiiiiiiiennnnn'], groupList: ['ouaaaaah', 'ceeeeest', 'biiiiiiiiennnnn']}}>
-                <Router history={history}>
-                  <Route path='/' component={App} >
-                    <IndexRoute component={Home}/>
-                  </Route>
-                </Router>
-              </SearchProvider>
-            </StoreProvider>
-        );
-    }
-}
+const Root = ({store, history}) => /*On place le provider de store au plus haut afin de pouvoir injecter des informations du store dans toute l'applciation.*/
+  <StoreProvider store={store}>
+    <SearchProvider store={store} searchMetadata={{getListMetadata : _getListMetadata, sortList: ['ouaaaaah', 'ceeeeest', 'biiiiiiiiennnnn'], groupList: ['ouaaaaah', 'ceeeeest', 'biiiiiiiiennnnn']}}>
+      <Router history={history}>
+        <Route path='/' component={App} >
+          <IndexRoute component={Home}/>
+        </Route>
+      </Router>
+    </SearchProvider>
+  </StoreProvider>
+;
 
-Root.displayName = 'Root';
 Root.propTypes = {
-    history: PropTypes.object.isRequired,
-    store: PropTypes.object.isRequired
+  history: PropTypes.object.isRequired
 };
+
 export default Root;
