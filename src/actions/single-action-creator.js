ed7d962e82fa9ed2298bacc37d63b76104af00e3
Thomas <tgallon@gmail.com>
2016-12-07 11:18:10 +0100

    fix scope casing

--- src/actions/single-action-creator.js
+++ src/actions/single-action-creator.js
@@ -14,85 +14,84 @@ const _validateUnitActionBuilderParams = (name) => {
 
 
 export const singleActionCreatorBuilder = name => {
-	_validateUnitActionBuilderParams(name);
-	const UPDATE_QUERY_SEARCH =  `${toUpper(name)}_UPDATE_QUERY`;
-  const UPDATE_SORT_SEARCH =  `${toUpper(name)}_UPDATE_SORT`;
-  const UPDATE_GROUP_SEARCH =  `${toUpper(name)}_UPDATE_GROUP`;
-  const UPDATE_SELECTED_FACETS_SEARCH =  `${toUpper(name)}_UPDATE_SELECTED_FACETS`;
-  const START_SEARCH = `${toUpper(name)}_START_SEARCH`;
+    _validateUnitActionBuilderParams(name);
+    const UPDATE_QUERY_SEARCH =  `${toUpper(name)}_UPDATE_QUERY`;
+    const UPDATE_SORT_SEARCH =  `${toUpper(name)}_UPDATE_SORT`;
+    const UPDATE_GROUP_SEARCH =  `${toUpper(name)}_UPDATE_GROUP`;
+    const UPDATE_SELECTED_FACETS_SEARCH =  `${toUpper(name)}_UPDATE_SELECTED_FACETS`;
+    const START_SEARCH = `${toUpper(name)}_START_SEARCH`;
 
-  return {
-    creators: {
-      updateQuery : updateQuery(UPDATE_QUERY_SEARCH),
-      updateSort : updateSort(UPDATE_SORT_SEARCH),
-      updateGroup : updateGroup(UPDATE_GROUP_SEARCH),
-      updateSelectedFacets: updateSelectedFacets(UPDATE_SELECTED_FACETS_SEARCH),
-      startSearch: startSearch(START_SEARCH)
-    },
-    types: {
-      [UPDATE_QUERY_SEARCH]: UPDATE_QUERY_SEARCH,
-      [UPDATE_SORT_SEARCH]: UPDATE_SORT_SEARCH,
-      [UPDATE_GROUP_SEARCH]: UPDATE_GROUP_SEARCH,
-      [UPDATE_SELECTED_FACETS_SEARCH]: UPDATE_SELECTED_FACETS_SEARCH,
-      [START_SEARCH]: START_SEARCH
+    return {
+        creators: {
+            updateQuery : updateQuery(UPDATE_QUERY_SEARCH),
+            updateSort : updateSort(UPDATE_SORT_SEARCH),
+            updateGroup : updateGroup(UPDATE_GROUP_SEARCH),
+            updateSelectedFacets: updateSelectedFacets(UPDATE_SELECTED_FACETS_SEARCH),
+            startSearch: startSearch(START_SEARCH)
+        },
+        types: {
+            [UPDATE_QUERY_SEARCH]: UPDATE_QUERY_SEARCH,
+            [UPDATE_SORT_SEARCH]: UPDATE_SORT_SEARCH,
+            [UPDATE_GROUP_SEARCH]: UPDATE_GROUP_SEARCH,
+            [UPDATE_SELECTED_FACETS_SEARCH]: UPDATE_SELECTED_FACETS_SEARCH,
+            [START_SEARCH]: START_SEARCH
+        }
     }
-  }
-
 }
 
 /**
- * Actions comments
- * @param  {query} search query
- * @return {object} the action itself
- */
+* Actions comments
+* @param  {query} search query
+* @return {object} the action itself
+*/
 
 const updateQuery = type => (query, replace = false, isSearchAction = true) => ({
-	type: type,
-	query: query,
-  replace,
-  isSearchAction
+    type: type,
+    query: query,
+    replace,
+    isSearchAction
 });
 
 
 const updateSort = type => (sort, replace = false, isSearchAction = true) => ({
-	type: type,
-	sort: sort,
-  replace,
-  isSearchAction
+    type: type,
+    sort: sort,
+    replace,
+    isSearchAction
 });
 const updateGroup = type => (group, replace = false, isSearchAction = true) => ({
-	type: type,
-	group: group,
-  replace,
-  isSearchAction
+    type: type,
+    group: group,
+    replace,
+    isSearchAction
 });
 
 /**
- * Actions comments
- * @param  {selectedFacets} search selectedFacets
- * @return {object} the action itself
- */
+* Actions comments
+* @param  {selectedFacets} search selectedFacets
+* @return {object} the action itself
+*/
 const updateSelectedFacets = type => (selectedFacets, replace = false, isSearchAction = true) => ({
-	type: type,
-	selectedFacets: selectedFacets,
-  replace,
-  isSearchAction
+    type: type,
+    selectedFacets: selectedFacets,
+    replace,
+    isSearchAction
 });
 
 
 const startSearch = type => () => ({
-  type : type,
-  isSearchAction: true
+    type : type,
+    isSearchAction: true
 })
 /*
 
 // High order action creator => not really readable ...
 cont hb = (type, property) => {
-  (propertyValue, replace) => ({
-    [property]: propertyValue,
-    replace,
-    type
-  })
+(propertyValue, replace) => ({
+[property]: propertyValue,
+replace,
+type
+})
 }
 
 hb(UPDATE_QUERY_SEARCH, 'selectedFacets')
