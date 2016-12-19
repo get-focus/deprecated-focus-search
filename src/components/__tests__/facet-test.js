7350b2f0377814d26080fed451f63c4875fd2a3e
Thomas <tgallon@gmail.com>
2016-12-19 16:37:59 +0100

    fix tests

--- src/components/__tests__/facet-test.js
+++ src/components/__tests__/facet-test.js
@@ -1,91 +1,92 @@
-// test utils
 import { mount, shallow } from 'enzyme';
-// Components
+import i18next from 'i18next';
 import {Facet, FacetPanel, FacetBlock,FacetTitle, FacetCount} from '../facet';
 
 describe('Facets components ', () => {
-  describe('<FacetTitle />', ()=> {
-    it('should be a span with a data-focus=facet-title', () => {
-      const wrapper = shallow(<FacetTitle />);
-      expect(wrapper.find('[data-focus="facet-title"]')).to.have.length(1);
+    describe('<FacetTitle />', ()=> {
+        it('should be a span with a data-focus=facet-title', () => {
+            const wrapper = shallow(<FacetTitle />);
+            expect(wrapper.find('[data-focus="facet-title"]')).to.have.length(1);
+        });
+        it('should add its children props', () => {
+            const wrapper = shallow(<FacetTitle><div>{'Amélie'}</div></FacetTitle>);
+            expect(wrapper.contains(<div>{'Amélie'}</div>)).to.be.true;
+        });
     });
-    it('should add its children props', () => {
-      const wrapper = shallow(<FacetTitle><div>{'Amélie'}</div></FacetTitle>);
-      expect(wrapper.contains(<div>{'Amélie'}</div>)).to.be.true;
+    describe('<FacetCount />', ()=> {
+        it('should be a div with a data-focus=facet-count', () => {
+            const wrapper = shallow(<FacetCount />);
+            expect(wrapper.find('[data-focus="facet-count"]')).to.have.length(1);
+        });
     });
-  });
-  describe('<FacetCount />', ()=> {
-    it('should be a div with a data-focus=facet-count', () => {
-      const wrapper = shallow(<FacetCount />);
-      expect(wrapper.find('[data-focus="facet-count"]')).to.have.length(1);
+    describe('<Facet />', () => {
+        it('should be a li with a data-focus=facet', () => {
+            const wrapper = shallow(<Facet />);
+            expect(wrapper.find('[data-focus="facet"]')).to.have.length(1);
+        });
+        it('should be a display the provided label', () => {
+            const wrapper = shallow(<Facet label='rodrigo'/>);
+            expect(wrapper.contains(<FacetTitle>{'rodrigo'}</FacetTitle>)).to.be.true;
+        });
+        it('should be a display the provided count', () => {
+            const wrapper = shallow(<Facet count={3}/>);
+            expect(wrapper.contains(<FacetCount>{3}</FacetCount>)).to.be.true;
+        });
+        it('should call the action on click', () => {
+            const selectFacetSpy =  sinon.spy();
+            const wrapper = shallow(<Facet code='MONKEY'count={3} label='papa' onClick={selectFacetSpy}/>)
+            wrapper.find('li').simulate('click');
+            expect(selectFacetSpy).to.have.property('callCount', 1);
+            expect(selectFacetSpy).to.have.been.calledWith('MONKEY')
+        })
     });
-  });
-  describe('<Facet />', () => {
-    it('should be a li with a data-focus=facet', () => {
-      const wrapper = shallow(<Facet />);
-      expect(wrapper.find('[data-focus="facet"]')).to.have.length(1);
+    describe('<FacetBlock />', () => {
+        it('should be a div with a data-focus=facet', () => {
+            const wrapper = shallow(<FacetBlock />);
+            expect(wrapper.find('[data-focus="facet-block"]')).to.have.length(1);
+        });
+        it('should be an title when a label is provided', () => {
+            i18next.init({resources: {translation: {search: {facet: {label:'This is a title'}}}}}, () => {
+                const wrapper = shallow(<FacetBlock label='label'/>);
+                expect(wrapper.contains(<h3>{i18next.t('search.facet.label')}</h3>)).to.be.true;
+            });
+        });
+        it('should be display all the facets provided into values', () => {
+            const FACET_VALUES = [
+                {code: 'FAIBLE', label: 'faible', count: 22},
+                {code: 'MOYEN', label: 'moyen', count: 54},
+                {code: 'FORT', label: 'fort', count: 7}
+            ];
+            const wrapper = mount(<FacetBlock label='don diego' values={FACET_VALUES}/>);
+            expect(wrapper.find('[data-focus="facet"]')).to.have.length(FACET_VALUES.length);
+        });
+        it('should use the FacetComponent provide in the props', () => {
+            const facetSpy = sinon.spy();
+            const SpyFacetComponent = props => {
+                facetSpy(props);
+                return <div>Facet</div>
+            }
+            const FACET_VALUES = [
+                {code: 'FAIBLE', label: 'faible', count: 22},
+                {code: 'MOYEN', label: 'moyen', count: 54},
+                {code: 'FORT', label: 'fort', count: 7}
+            ];
+            const wrapper = mount(<FacetBlock label='don diego' values={FACET_VALUES} FacetComponent={SpyFacetComponent}/>);
+            expect(facetSpy).to.have.callCount(3);
+        });
     });
-    it('should be a display the provided label', () => {
-      const wrapper = shallow(<Facet label='rodrigo'/>);
-      expect(wrapper.contains(<FacetTitle>{'rodrigo'}</FacetTitle>)).to.be.true;
+    describe('<FacetPanel />', () => {
+        it('should be a div with a data-focus=facet-panel', () => {
+            const wrapper = shallow(<FacetPanel i18n={elm => elm} />);
+            expect(wrapper.find('[data-focus="facet-panel"]')).to.have.length(1);
+        });
+        it.skip('should add the title given as props', () => {
+            const wrapper = shallow(<FacetPanel i18n={elm => elm}  title='Great title'/>);
+            expect(wrapper.contains(<h4>{'focus.search.facets'}</h4>)).to.be.true;
+        });
+        it('should add the facet-block given as data in the props', () => {
+            const wrapper = mount(<FacetPanel i18n={elm => elm}  title='Great title' data={[{values: ['1'], selected: true}, {values: ['2'], selected: true}, {values: ['3'], selected: true}]}/>);
+            expect(wrapper.find('[data-focus="facet-block"]')).to.have.length(3);
+        });
     });
-    it('should be a display the provided count', () => {
-      const wrapper = shallow(<Facet count={3}/>);
-      expect(wrapper.contains(<FacetCount>{3}</FacetCount>)).to.be.true;
-    });
-    it('should call the action on click', () => {
-      const selectFacetSpy =  sinon.spy();
-      const wrapper = shallow(<Facet code='MONKEY'count={3} label='papa' onClick={selectFacetSpy}/>)
-      wrapper.find('li').simulate('click');
-      expect(selectFacetSpy).to.have.property('callCount', 1);
-      expect(selectFacetSpy).to.have.been.calledWith('MONKEY')
-    })
-  });
-  describe('<FacetBlock />', () => {
-    it('should be a div with a data-focus=facet', () => {
-      const wrapper = shallow(<FacetBlock />);
-      expect(wrapper.find('[data-focus="facet-block"]')).to.have.length(1);
-    });
-    it('should be an title when a label is provided', () => {
-      const wrapper = shallow(<FacetBlock label='don diego'/>);
-      expect(wrapper.contains(<h3>{'don diego'}</h3>)).to.be.true;
-    });
-    it('should be display all the facets provided into values', () => {
-      const FACET_VALUES = [
-        {code: 'FAIBLE', label: 'faible', count: 22},
-        {code: 'MOYEN', label: 'moyen', count: 54},
-        {code: 'FORT', label: 'fort', count: 7}
-      ];
-      const wrapper = mount(<FacetBlock label='don diego' values={FACET_VALUES}/>);
-      expect(wrapper.find('[data-focus="facet"]')).to.have.length(FACET_VALUES.length);
-    });
-    it('should use the FacetComponent provide in the props', () => {
-      const facetSpy = sinon.spy();
-      const SpyFacetComponent = props => {
-        facetSpy(props);
-        return <div>Facet</div>
-      }
-      const FACET_VALUES = [
-        {code: 'FAIBLE', label: 'faible', count: 22},
-        {code: 'MOYEN', label: 'moyen', count: 54},
-        {code: 'FORT', label: 'fort', count: 7}
-      ];
-      const wrapper = mount(<FacetBlock label='don diego' values={FACET_VALUES} FacetComponent={SpyFacetComponent}/>);
-      expect(facetSpy).to.have.callCount(3);
-    });
-  });
-  describe('<FacetPanel />', () => {
-    it('should be a div with a data-focus=facet-panel', () => {
-      const wrapper = shallow(<FacetPanel i18n={elm => elm} />);
-      expect(wrapper.find('[data-focus="facet-panel"]')).to.have.length(1);
-    });
-    it.skip('should add the title given as props', () => {
-      const wrapper = shallow(<FacetPanel i18n={elm => elm}  title='Great title'/>);
-      expect(wrapper.contains(<h4>{'focus.search.facets'}</h4>)).to.be.true;
-    });
-    it('should add the facet-block given as data in the props', () => {
-      const wrapper = mount(<FacetPanel i18n={elm => elm}  title='Great title' data={[{values: ['1'], selected: true}, {values: ['2'], selected: true}, {values: ['3'], selected: true}]}/>);
-      expect(wrapper.find('[data-focus="facet-block"]')).to.have.length(3);
-    });
-  });
 })
