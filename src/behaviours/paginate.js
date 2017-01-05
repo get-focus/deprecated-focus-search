d0705423d7732bc65590bd3bea7ab9d47fef34d7
Thomas <tgallon@gmail.com>
2017-01-05 17:15:34 +0100

    fix isOtherAction props

--- src/behaviours/paginate.js
+++ src/behaviours/paginate.js
@@ -15,7 +15,7 @@ export default () => {
                 this._otherAction = this._otherAction.bind(this);
             }
             _onClickNext() {
-                const { onClickNext, top, skip, page} = this.props;
+                const { onClickNext, top, skip, page } = this.props;
                 const newTop = this.state.top + page;
                 this.setState({
                     top: newTop
@@ -23,22 +23,23 @@ export default () => {
                 onClickNext(newTop, skip);
             }
             _otherAction(){
-              const {otherAction} = this.props;
-              window.scrollTo(0,0);
-              otherAction({...this.props, ...this.state})
+                const {otherAction} = this.props;
+                window.scrollTo(0,0);
+                otherAction({...this.props, ...this.state})
             }
             render() {
                 const {top} = this.state;
-                const {totalCount, isOtherAction, data} = this.props;
+                const {totalCount, otherAction, data} = this.props;
+                const isOtherAction = otherAction !== null;
                 return (
                     <div data-focus='list-with-pagination'>
                         <ComponentToConnect {...this.props} />
                         <div data-focus='pagination'>
-                          <div data-focus='pagination-indicators'>{(data && data.length < top) ? data.length : top} {i18next.t(`search.paginate.totalCount`)}</div>
-                          <div data-focus='pagination__actions'>
-                              {!isOtherAction && <Button data-focus='paginate.show.next' label='search.paginate.show.next' onClick={this._onClickNext} />}
-                              {isOtherAction && <Button data-focus='paginate.other.action' label='search.paginate.other.action' onClick={this._otherAction} />}
-                          </div>
+                            <div data-focus='pagination-indicators'>{(data && data.length < top) ? data.length : top} {i18next.t(`search.paginate.totalCount`)}</div>
+                            <div data-focus='pagination__actions'>
+                                {!isOtherAction && <Button data-focus='paginate.show.next' label='search.paginate.show.next' onClick={this._onClickNext} />}
+                                {isOtherAction && <Button data-focus='paginate.other.action' label='search.paginate.other.action' onClick={this._otherAction} />}
+                            </div>
                         </div>
                     </div>
                 );
@@ -46,21 +47,18 @@ export default () => {
         }
         PaginationConnector.displayName = 'PaginationConnector';
         PaginationConnector.defaultProps = {
-          page : 10,
-          skip : 0,
-          top : 10,
-          otherAction : (params) => console.log("please define a other function for the 'other action'. The passed params are " + JSON.stringify(params)),
-          onClickNext : (params) => console.log('please define a function. The passed params are ' + JSON.stringify(params)),
-          isOtherAction : false
-
+            onClickNext: (params) => console.log('please define a function. The passed params are ' + JSON.stringify(params)),
+            otherAction: (params) => console.log("please define a other function for the 'other action'. The passed params are " + JSON.stringify(params)),
+            page: 10,
+            skip: 0,
+            top: 10
         }
         PaginationConnector.propTypes = {
-          page: PropTypes.number,
-          skip: PropTypes.nnumber,
-          top: PropTypes.number,
-          isOtherAction: PropTypes.bool,
-          otherAction: PropTypes.func,
-          onClickNext: PropTypes.func.isRequired
+            onClickNext: PropTypes.func.isRequired,
+            otherAction: PropTypes.func,
+            page: PropTypes.number,
+            skip: PropTypes.nnumber,
+            top: PropTypes.number
         }
         return PaginationConnector;
     }
